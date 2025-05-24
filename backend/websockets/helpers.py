b136315dd1898e4607a5e73cf98eea4dddcb5d59
FREDY129053 <fredy1290534518@gmail.com>
2025-05-24 21:32:34 +1000

    fix: код вебсокетов

---
+++ backend/websockets/helpers.py
@@ -0,0 +1,209 @@
+from typing import List, Tuple
+
+import httpx
+
+API_BASE_URL = "http://localhost:8080/api/rooms"
+
+
+# Заросы по комнате
+
+
+async def is_user_in_room(user_id: str, room_id: str) -> bool:
+    """Проверка нахождения пользователя в комнате
+
+    Args:
+        user_id (str): идентификатор пользователя
+        room_id (str): идентификатор комнаты
+
+    Returns:
+        bool: есть ли пользователь в комнате (да/нет)
+    """
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                f"{API_BASE_URL}/is_in_room",
+                params={"user_uuid": user_id, "room_uuid": room_id},
+            )
+            response.raise_for_status()
+            return response.json()["result"] is True
+    except Exception as e:
+        print(f"\033[031m[ERROR]\033[037m\t  Проверка is_in_room не удалась: {e}")
+        return False
+
+
+async def add_user_to_room(user_id: str, room_id: str):
+    """Добавление пользователя в комнату
+
+    Args:
+        user_id (str): идентификатор пользователя
+        room_id (str): идентификатор комнаты
+    """
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                f"{API_BASE_URL}/add_member",
+                json={"user_uuid": user_id, "room_uuid": room_id},
+            )
+            response.raise_for_status()
+    except Exception as e:
+        print(
+            f"\033[031m[ERROR]\033[037m\t  Не удалось добавить пользователя в комнату: {e}"
+        )
+
+
+async def remove_user_from_room(user_id: str, room_id: str):
+    """Удаление пользователя из комнаты
+
+    Args:
+        user_id (str): идентификатор пользователя
+        room_id (str): идентификатор комнаты
+    """
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.delete(
+                f"{API_BASE_URL}/delete_member",
+                params={"user_uuid": user_id, "room_uuid": room_id},
+            )
+            response.raise_for_status()
+    except Exception as e:
+        print(
+            f"\033[031m[ERROR]\033[037m\t  Не удалось удалить пользователя из комнаты: {e}"
+        )
+
+
+async def update_user_stats(user_id: str, type: str):
+    """Обновление статистики игрока
+
+    Args:
+        user_id (str): идентификатор пользователя
+        type (str): результат для обновления (wins/losses/draws)
+    """
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.patch(
+                "http://localhost:8080/api/users/update_stats",
+                json={"user_uuid": user_id, "stat_type": type},
+            )
+            response.raise_for_status()
+    except Exception as e:
+        print(
+            f"\033[031m[ERROR]\033[037m\t  Не удалось обновить ститистику пользователя: {e}"
+        )
+
+
+# Запросы по игре
+
+
+async def add_game(room_id: str, game_id: str, is_hard: bool):
+    """Создание игры в комнате
+
+    Args:
+        room_id (str): идентификатор комнаты
+        game_id (str): идентификатор игры
+        is_hard (bool): сложность игры (да/нет)
+    """
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                f"{API_BASE_URL}/{room_id}/start_game?generated_id={game_id}&is_hard={is_hard}",
+            )
+            response.raise_for_status()
+    except Exception as e:
+        print(f"\033[031m[ERROR]\033[037m\t  Не удалось создать игру: {e}")
+
+
+async def write_game_result(game_id: str, user_id: str, opponent_id: str, result: str):
+    """Запись результата игры
+
+    Args:
+        game_id (str): идентификатор игры
+        user_id (str): идентификатор игрока
+        opponent_id (str): идентификатор оппонента игрока
+        result (str): результат игры (win/lose/draw)
+    """
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                f"{API_BASE_URL}/games/{game_id}/result",
+                json={
+                    "user_uuid": user_id,
+                    "opponent_uuid": opponent_id,
+                    "result": result,
+                },
+            )
+            response.raise_for_status()
+    except Exception as e:
+        print(f"\033[031m[ERROR]\033[037m\t  Не удалось добавить результат: {e}")
+
+
+async def update_game(game_id: str):
+    """Обновление данных игры.
+    Сейчас: запись окончания игры
+
+    Args:
+        game_id (str): идентификатор игры
+    """
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.patch(
+                f"{API_BASE_URL}/games/{game_id}",
+            )
+            response.raise_for_status()
+    except Exception as e:
+        print(f"\033[031m[ERROR]\033[037m\t  Не удалось обновить игру: {e}")
+
+
+async def delete_game(game_id: str):
+    """Удаление игры
+
+    Args:
+        game_id (str): идентификатор игры
+    """
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.delete(
+                f"{API_BASE_URL}/games/{game_id}",
+            )
+            response.raise_for_status()
+    except Exception as e:
+        print(f"\033[031m[ERROR]\033[037m\t  Не удалось удалить игру: {e}")
+
+
+def check_draw(field: List[str]) -> bool:
+    """Проверка текущего поля на состояние "Ничья"
+
+    Args:
+        field (List[str]): текущее поле игры
+
+    Returns:
+        bool: есть ничья или нет
+    """
+    return all(cell != "" for cell in field)
+
+
+def check_winner(field: List[str]) -> Tuple[bool, List[int]]:
+    """Проверка текущего поля игры на победу
+
+    Args:
+        field (List[str]): текущее поле игры
+
+    Returns:
+        Tuple[bool, List[int]]: победа или нет + выигрышная комбинация
+    """
+    winning_combs = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    for comb in winning_combs:
+        a, b, c = comb
+        if field[a] != "" and field[a] == field[b] == field[c]:
+            return True, comb
+
+    return False, []
--- backend/websockets/socket_manager.py
+++ backend/websockets/socket_manager.py
@@ -1,40 +1,75 @@
 import asyncio
-from typing import Dict, List, Set, Tuple
+from typing import Dict, List, Set
 from uuid import uuid4
 
-import httpx
 from fastapi import WebSocket
+
+from backend.websockets.helpers import (
+    add_game,
+    add_user_to_room,
+    check_draw,
+    check_winner,
+    delete_game,
+    is_user_in_room,
+    remove_user_from_room,
+    update_game,
+    update_user_stats,
+    write_game_result,
+)
 
 API_BASE_URL = "http://localhost:8080/api/rooms"
 
 
 class ConnectionManager:
-    """TODO: docstring нахерачить"""
+    """Менеджер действий для Websocket"""
 
     def __init__(self):
+        # Текущие подключения для каждого игрока
         self.clientConnections: Dict[str, WebSocket] = {}
+        # Проголосовавшие "за" в каждой комнате
         self.ready_votes: Dict[str, Set[str]] = {}
+        # Оппонент для каждого игрока
         self.opponents: Dict[str, str] = {}
+        # Информация о комнате и игроках в ней
         self.rooms: Dict[str, List[str]] = {}
+        # Сложность комнаты (да/нет)
         self.is_difficult: bool = False
+        # Хранение игры для комнаты со статусом активности
         self._room_and_game_data: Dict[str, Dict[str, bool]] = {}
 
     async def connect(
         self, websocket: WebSocket, room_id: str, user_id: str, difficult: bool
     ):
+        """Подключение пользователя к комнате
+
+        Args:
+            websocket (WebSocket): подключение
+            room_id (str): идентификатор комнаты
+            user_id (str): идентификатор пользователя
+            difficult (bool): сложность игры (да/нет)
+        """
         await websocket.accept()
+        # Сохраняем принятое подключение для отправки сообщений
         self.clientConnections[user_id] = websocket
         self.is_difficult = difficult
 
-        if not await self._is_user_in_room(user_id, room_id):
-            await self._add_user_to_room(user_id, room_id)
+        # Если пользователя нет в комнате, то записываем его
+        if not await is_user_in_room(user_id, room_id):
+            await add_user_to_room(user_id, room_id)
 
+        # Подбираем оппонента
         await self._match_clients(user_id, room_id)
 
     async def disconnect(self, user_id: str):
+        """Отключение игрока
+
+        Args:
+            user_id (str): идентификатор игрока
+        """
         if user_id in self.clientConnections:
             del self.clientConnections[user_id]
 
+            # Отправка сообщения оппоненту, что пользователь вышел
             opponent_id = self.opponents.get(user_id)
             if opponent_id and opponent_id in self.clientConnections:
                 asyncio.create_task(
@@ -48,31 +83,41 @@ class ConnectionManager:
 
             self.opponents.pop(user_id, None)
             self.opponents.pop(opponent_id, None)  # type: ignore
+            # Удаляем данные об отключенном пользователе из комнаты
             for room_id, room in self.rooms.items():
                 if user_id in room:
                     room.remove(user_id)
-                    asyncio.create_task(self._remove_user_from_room(user_id, room_id))
+                    asyncio.create_task(remove_user_from_room(user_id, room_id))
 
+                    # Удаление данных об игре при отключении
                     game_data = self._room_and_game_data[room_id]
                     if next(iter(game_data.values())):
-                        asyncio.create_task(
-                            self._delete_game(next(iter(game_data.keys())))
-                        )
+                        asyncio.create_task(delete_game(next(iter(game_data.keys()))))
 
     async def _match_clients(self, user_id: str, room_id: str):
+        """Подбор игроков друг другу в одной комнате
+
+        Args:
+            user_id (str): идентификатор пользователя
+            room_id (str): идентификатор комнаты
+        """
+        # Создаем данные об игроках в комнате, если ее не было
         if room_id not in self.rooms:
             self.rooms[room_id] = []
         self.rooms[room_id].append(user_id)
 
+        # Если игроков не достаточно, то ничего не делаем
         if len(self.rooms[room_id]) < 2:
             return
 
+        # Задаем оппонентов в комнате
         player_2 = self.rooms[room_id][-1]
         player_1 = self.rooms[room_id][-2]
 
         self.opponents[player_1] = player_2
         self.opponents[player_2] = player_1
 
+        # Шлем сообщения игрокам в комнате
         await self._send(
             player_1,
             {
@@ -87,109 +132,24 @@ class ConnectionManager:
         )
 
     async def _send(self, user_id: str, message: Dict[str, str | int]):
+        """Отправка сообщений пользователю
+
+        Args:
+            user_id (str): идентификатор пользователя
+            message (Dict[str, str  |  int]): сообщение для сериализации в JSON
+        """
         ws = self.clientConnections.get(user_id)
         if ws:
             await ws.send_json(message)
 
-    # HTTP запросы
-    # Заросы по комнате
-    async def _is_user_in_room(self, user_id: str, room_id: str) -> bool:
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.get(
-                    f"{API_BASE_URL}/is_in_room",
-                    params={"user_uuid": user_id, "room_uuid": room_id},
-                )
-                response.raise_for_status()
-                return response.json()["result"] is True
-        except Exception as e:
-            print(f"[ERROR] Проверка is_in_room не удалась: {e}")
-            return False
-
-    async def _add_user_to_room(self, user_id: str, room_id: str):
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.post(
-                    f"{API_BASE_URL}/add_member",
-                    json={"user_uuid": user_id, "room_uuid": room_id},
-                )
-                response.raise_for_status()
-        except Exception as e:
-            print(f"[ERROR] Не удалось добавить пользователя в комнату: {e}")
-
-    async def _remove_user_from_room(self, user_id: str, room_id: str):
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.delete(
-                    f"{API_BASE_URL}/delete_member",
-                    params={"user_uuid": user_id, "room_uuid": room_id},
-                )
-                response.raise_for_status()
-        except Exception as e:
-            print(f"[ERROR] Не удалось удалить пользователя из комнаты: {e}")
-
-    async def _update_user_stats(self, user_id: str, type: str):
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.patch(
-                    "http://localhost:8080/api/users/update_stats",
-                    json={"user_uuid": user_id, "stat_type": type},
-                )
-                response.raise_for_status()
-        except Exception as e:
-            print(f"[ERROR] Не удалось обновить ститистику пользователя: {e}")
-
-    # HTTP запросы
-    # Запросы по самим играм
-    async def _add_game(self, room_id: str, game_id: str, is_hard: bool):
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.post(
-                    f"{API_BASE_URL}/{room_id}/start_game?generated_id={game_id}&is_hard={is_hard}",
-                )
-                response.raise_for_status()
-        except Exception as e:
-            print(f"[ERROR] Не удалось создать игру: {e}")
-
-    async def _write_game_result(
-        self, game_id: str, user_id: str, opponent_id: str, result: str
-    ):
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.post(
-                    f"{API_BASE_URL}/games/{game_id}/result",
-                    json={
-                        "user_uuid": user_id,
-                        "opponent_uuid": opponent_id,
-                        "result": result,
-                    },
-                )
-                response.raise_for_status()
-        except Exception as e:
-            print(f"[ERROR] Не удалось добавить результат: {e}")
-
-    async def _update_game(self, game_id: str):
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.patch(
-                    f"{API_BASE_URL}/games/{game_id}",
-                )
-                response.raise_for_status()
-        except Exception as e:
-            print(f"[ERROR] Не удалось обновить игру: {e}")
-
-    async def _delete_game(self, game_id: str):
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.delete(
-                    f"{API_BASE_URL}/games/{game_id}",
-                )
-                response.raise_for_status()
-        except Exception as e:
-            print(f"[ERROR] Не удалось удалить игру: {e}")
-
     # Ходы
     async def handle_game_start(self, user_id: str, room_id: str):
+        """Активация игры путем голосования
+
+        Args:
+            user_id (str): идентификатор пользователя
+            room_id (str): идентификатор комнаты
+        """
         if room_id not in self.ready_votes:
             self.ready_votes[room_id] = set()
 
@@ -203,6 +163,7 @@ class ConnectionManager:
         await self._send(player_1, {"method": "ready_vote", "votes": votes_count})
         await self._send(player_2, {"method": "ready_vote", "votes": votes_count})
 
+        # Если готовы все, то начинаем игру
         if votes_count >= 2:
             game_uuid = str(uuid4())
             self._room_and_game_data[room_id] = {game_uuid: True}
@@ -228,21 +189,28 @@ class ConnectionManager:
                 },
             )
 
-            await self._add_game(
-                room_id=room_id, game_id=game_uuid, is_hard=self.is_difficult
-            )
+            await add_game(room_id=room_id, game_id=game_uuid, is_hard=self.is_difficult)
 
     async def handle_move(self, user_id: str, data: Dict[str, str | int], room_id: str):
+        """Ходы во время игры
+
+        Args:
+            user_id (str): идентификатор пользователя
+            data (Dict[str, str  |  int]): данные о ходе игрока
+            room_id (str): идентификатор комнаты
+        """
         opponent_id = self.opponents.get(user_id)
         if not opponent_id:
             return
 
+        # Получаем нужные данные
         field = data.get("field", [])
         symbol = data.get("symbol")
         game_data = self._room_and_game_data[room_id]
         game_id = next(iter(game_data.keys()))
 
-        is_winner, comb = self._check_winner(field)  # type: ignore
+        is_winner, comb = check_winner(field)  # type: ignore
+        # Если победа, то выдаем результат
         if is_winner:
             result_message = {
                 "method": "result",
@@ -253,20 +221,21 @@ class ConnectionManager:
             self._room_and_game_data[room_id][game_id] = False
             await self._send(user_id, result_message)
             await self._send(opponent_id, result_message)
-            await self._update_user_stats(user_id, "wins")
-            await self._update_user_stats(opponent_id, "losses")
+            await update_user_stats(user_id, "wins")
+            await update_user_stats(opponent_id, "losses")
 
-            await self._update_game(game_id)
-            await self._write_game_result(
+            await update_game(game_id)
+            await write_game_result(
                 game_id=game_id, user_id=user_id, opponent_id=opponent_id, result="win"
             )
-            await self._write_game_result(
+            await write_game_result(
                 game_id=game_id, user_id=opponent_id, opponent_id=user_id, result="lose"
             )
 
             return
 
-        if self._check_draw(field):  # type: ignore
+        if check_draw(field):  # type: ignore
+            # Если ничья, то выдаем результат
             draw_message = {
                 "method": "result",
                 "message": "Ничья",
@@ -277,19 +246,20 @@ class ConnectionManager:
             self._room_and_game_data[room_id][game_id] = False
             await self._send(user_id, draw_message)
             await self._send(opponent_id, draw_message)
-            await self._update_user_stats(user_id, "draws")
-            await self._update_user_stats(opponent_id, "draws")
+            await update_user_stats(user_id, "draws")
+            await update_user_stats(opponent_id, "draws")
 
-            await self._update_game(game_id)
-            await self._write_game_result(
+            await update_game(game_id)
+            await write_game_result(
                 game_id=game_id, user_id=user_id, opponent_id=opponent_id, result="draw"
             )
-            await self._write_game_result(
+            await write_game_result(
                 game_id=game_id, user_id=opponent_id, opponent_id=user_id, result="draw"
             )
 
             return
 
+        # Если игра в процессе, то продолжаем играть
         update_message = {
             "method": "update",
             "turn": "O" if symbol == "X" else "X",
@@ -299,25 +269,3 @@ class ConnectionManager:
         await self._send(opponent_id, update_message)
 
         return
-
-    def _check_draw(self, field: List[str]) -> bool:
-        return all(cell != "" for cell in field)
-
-    def _check_winner(self, field: List[str]) -> Tuple[bool, List[int]]:
-        winning_combs = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ]
-
-        for comb in winning_combs:
-            a, b, c = comb
-            if field[a] != "" and field[a] == field[b] == field[c]:
-                return True, comb
-
-        return False, []
